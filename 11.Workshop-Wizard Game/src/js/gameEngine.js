deaa368cf1c4242052844debedb4502121ae7461
Doncho Todorov <108018258+TodoroffWILL@users.noreply.github.com>
2023-02-22 13:18:09 +0200

    Adding wizard movement

--- 11.Workshop-Wizard Game/src/js/gameEngine.js
+++ 11.Workshop-Wizard Game/src/js/gameEngine.js
@@ -4,7 +4,27 @@ function start(state, game) {
 }
 
 function gameLoop(state, game) {
-  console.log(state.keys);
+  const { wizard } = state;
+  const { wizardElement } = game;
+
+  // Move wizard;
+
+  if (state.keys.KeyD) {
+    wizard.posX += wizard.speed;
+  }
+  if (state.keys.KeyW) {
+    wizard.posY -= wizard.speed;
+  }
+  if (state.keys.KeyS) {
+    wizard.posY += wizard.speed;
+  }
+  if (state.keys.KeyA) {
+    wizard.posX -= wizard.speed;
+  }
+  wizardElement.style.left = wizard.posX + 'px';
+  wizardElement.style.top = wizard.posY + 'px';
+
+  // Render
 
   window.requestAnimationFrame(gameLoop.bind(null, state, game));
 }
--- 11.Workshop-Wizard Game/src/js/gameState.js
+++ 11.Workshop-Wizard Game/src/js/gameState.js
@@ -1,13 +1,17 @@
 function initState() {
+  let startX = Math.floor(Math.random() * 1000);
+  let startY = Math.floor(Math.random() * 500);
+
   const state = {
     player: 'Doncho',
     wizard: {
       width: 82,
       height: 100,
-      startX: Math.floor(Math.random() * 1000),
-      startY: Math.floor(Math.random() * 500),
+      posX: startX,
+      posY: startY,
+      speed: 10,
     },
-    keys:{},
+    keys: {},
   };
   return state;
 }
