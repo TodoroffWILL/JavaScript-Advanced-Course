78d7c2b51006a03db1a6e4c18be44ccaf45ed8f6
Doncho Todorov <108018258+TodoroffWILL@users.noreply.github.com>
2023-02-22 12:11:50 +0200

    Create wizard with inital position on start

--- 11.Workshop-Wizard Game/src/js/gameState.js
+++ 11.Workshop-Wizard Game/src/js/gameState.js
@@ -1,10 +1,12 @@
-function initState (){
-    const state = {
-        player: 'Doncho',
-        
-
-
-
-    }
-    return state;
+function initState() {
+  const state = {
+    player: 'Doncho',
+    wizard: {
+      width: 82,
+      height: 100,
+      startX: Math.floor(Math.random() * 1000),
+      startY: Math.floor(Math.random() * 500),
+    },
+  };
+  return state;
 }
