4d123e14298f7ff72b96df7c9cb7eaa3340b1ede
Doncho Todorov <108018258+TodoroffWILL@users.noreply.github.com>
2023-02-22 21:34:43 +0200

    Add collision to fireball and bug

--- 11.Workshop-Wizard Game/src/js/gameEngine.js
+++ 11.Workshop-Wizard Game/src/js/gameEngine.js
@@ -15,19 +15,41 @@ function gameLoop(state, game, timestamp) {
   } else {
     wizardElement.style.backgroundImage = "url('./css/images/wizard.png')";
   }
+  // Detect fireball collision
 
+  // Spawn bugs
   if (timestamp > state.bugStats.nextSpawnTimestamp) {
     game.createBug(state.bugStats);
     state.bugStats.nextSpawnTimestamp =
       timestamp + Math.random() * state.bugStats.maxSpawnInterval;
   }
   // Render bugs
-  document.querySelectorAll('.bug').forEach((bug) => {
+  let bugElements = document.querySelectorAll('.bug');
+  bugElements.forEach((bug) => {
     let posX = parseInt(bug.style.left);
     if (posX > 0) {
       bug.style.left = posX - state.bugStats.speed + 'px';
     } else {
       bug.remove();
+    }
+  });
+
+  // Render Fireballs
+  document.querySelectorAll('.fireball').forEach((fireball) => {
+    let posX = parseInt(fireball.style.left);
+
+    // Detect collision
+    bugElements.forEach((bug) => {
+      if (detectCollision(bug, fireball)) {
+        bug.remove();
+        fireball.remove();
+      }
+    });
+
+    if (posX > game.gameScreen.offsetWidth) {
+      fireball.remove();
+    } else {
+      fireball.style.left = posX + state.fireball.speed + 'px';
     }
   });
 
@@ -58,4 +80,16 @@ function modifyWizardPosition(state, game) {
   if (state.keys.KeyA) {
     wizard.posX = Math.max(wizard.posX - wizard.speed, 0);
   }
+}
+function detectCollision(objectA, objectB) {
+  let first = objectA.getBoundingClientRect();
+  let second = objectB.getBoundingClientRect();
+
+  let hasCollision = !(
+    first.top > second.bottom ||
+    first.bottom < second.top ||
+    first.right < second.left ||
+    first.left > second.right
+  );
+  return hasCollision;
 }
